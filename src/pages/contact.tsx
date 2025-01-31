09045dba94cd7b8d808c47050614c16332fa0697
choxkri <1059788@hr.nl>
2025-01-31 14:49:25 +0100

    basic set up contact page and installed react-router ("npm i react-router-dom")

---
+++ src/pages/contact.tsx
@@ -0,0 +1,14 @@
+import react, { useEffect } from "react";
+import { useState } from 'react';
+
+
+
+function Contact(){
+    return(
+        <div>
+            <h2 className="App">Default contact page</h2>
+        </div>
+    );
+}
+
+export default Contact;
